8d2c7f67decae82a886ffd7537711a3b4f6d125a
Allen McCabe <allenmccabe@gmail.com>
2021-07-22 16:38:56 -0700

    Fix UsesDatabase trait.

--- tests/UsesDatabase.php
+++ tests/UsesDatabase.php
@@ -2,11 +2,15 @@
 
 namespace Tests;
 
+use PhpCli\Application;
+use PhpCli\Database\Driver;
 use PhpCli\Database\Query;
 use PhpCli\Filesystem\File;
 
 trait UsesDatabase
 {
+    public Application $app;
+
     public $database;
 
     public $db;
@@ -18,15 +22,20 @@ trait UsesDatabase
 
     protected function setUpDatabase(): \PDO
     {
-        $db = new \PDO(sprintf('sqlite:%s', $this->getDatabaseFile()->getPath()), '', '', array(
+        if (!isset($this->app)) {
+            $this->app = new Application();
+        }
+
+        $this->db = new \PDO(sprintf('sqlite:%s', $this->getDatabaseFile()->getPath()), '', '', array(
             \PDO::ATTR_EMULATE_PREPARES => false,
             \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
         ));
 
-        Query::setDriver($db);
+        $this->app->bindInstance(Driver::class, $this->db);
+        Query::app($this->app);
 
-        return $db;
+        return $this->db;
     }
 
     protected function tearDownDatabase(): void
