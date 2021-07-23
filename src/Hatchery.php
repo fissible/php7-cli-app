2905d492aa4b0f5150a440548a0b7144ec401663
Allen McCabe <allenmccabe@gmail.com>
2021-07-22 18:28:22 -0700

    Update Hatchery command.

--- src/Hatchery.php
+++ src/Hatchery.php
@@ -18,8 +18,9 @@ class Hatchery extends Application
      */
     public function doHatch()
     {
+        $name = $this->prompt('New CLI app name (eg. Hatchery): ', null, ['required']);
+
         // @todo Install autocompletion script? /etc/bash_completion.d/
-        $fileContent = $this->getBinaryContent();
         $saveTo = $this->prompt('Write binary to (directory): ');
 
         if (empty($saveTo)) {
@@ -27,15 +28,13 @@ class Hatchery extends Application
         }
 
         if (substr($saveTo, -1, 1) === DIRECTORY_SEPARATOR) {
-            $grey = Output::color($saveTo, 'light_purple');
-            $binary = $this->prompt('Name of binary: ' . $grey);
+            $binary = $this->prompt('Name of binary: ' . Output::color($saveTo, 'light_purple'));
             $saveTo .= $binary;
         }
 
         if (is_dir($saveTo)) {
             $saveTo = rtrim($saveTo, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
-            $grey = Output::color($saveTo, 'light_purple');
-            $binary = $this->prompt('Name of binary: ' . $grey);
+            $binary = $this->prompt('Name of binary: ' . Output::color($saveTo, 'light_purple'));
             $saveTo .= $binary;
         }
 
@@ -50,52 +49,60 @@ class Hatchery extends Application
             $this->exit();
         }
 
+        $fileContent = $this->getBinaryContent($name, $binary);
+
         if ($File->write($fileContent)) {
             $this->linef('New binary written to %s', $saveTo);
             $File->chmod(0755);
         }
     }
 
-    private function getBinaryContent()
+    private function getBinaryContent(string $name, string $binary)
     {
-        $name = $this->prompt('New CLI app name (eg. Hatchery): ', null, true);
+        
         $className = ucfirst($name);
         $commandClassName = 'Run' . $className . '';
         $phpVersionMinimum = $this->prompt('Minimum PHP version required: ', PHP_VERSION);
         $phpVersionMaximum = $this->prompt('Maximum PHP version allowed [none]: ');
         $path = dirname(dirname(__FILE__));
         $options = [];
-        $optionsString = '';
+        $optionsString = '$options';
         $arguments = [];
-        $argumentsString = '';
+        $argumentsString = '$arguments';
+
+        $this->linef('Now you can define options/flags and arguments, eg. %s [options...] <argument>', $binary);
 
         // Define Options
-        if ($this->promptYesNo('Do you want to define options/flags interactively? [yes/No]', false)) {
+        if ($this->promptYesNo('Do you want to define options/flags interactively? ', false)) {
             $adding = true;
             while ($adding) {
                 $options[] = [
-                    'name' => $this->prompt('What is the option name? (eg. "file", "f", or "f|file"): ', null, true),
-                    'requiresValue' => $this->prompt('Does this option require a value or is it a flag? [yes/no/Flag]: ', null, false),
-                    'description' => $this->prompt('Please provide a description for the help function: ', null, false),
-                    'defaultValue' => $this->prompt('Please provide a default value [N/A]: ', null, false)
+                    'name' => $this->prompt('What is the option name? (eg. "file", "f", or "f|file"): ', null, ['required']),
+                    'requiresValue' => $this->prompt('Does this option require a value or is it a flag? [yes/no/Flag]: '),
+                    'description' => $this->prompt('Please provide a description for the help function: '),
+                    'defaultValue' => $this->prompt('Please provide a default value [N/A]: ')
                 ];
 
-                $adding = $this->promptYesNo('Do you want to define another option or flag? [Yes/no]', true);
+                $adding = $this->promptYesNo('Do you want to define another option or flag? ', true);
             }
         }
 
         // Define Arguments
         if ($this->promptYesNo('Do you want to define arguments interactively? ', false)) {
-            $arguments[] = [
-                'name' => $this->prompt('What is the argument name? (eg. "path"): ', null, true),
-                'requiresValue' => $this->promptYesNo('Is this argument required? [yes/No]: ', false),
-                'defaultValue' => $this->prompt('Please provide a default value [N/A]: ', null, false)
-            ];
+            $adding = true;
+            while ($adding) {
+                $arguments[] = [
+                    'name' => $this->prompt('What is the argument name? (eg. "path"): ', null, ['required']),
+                    'requiresValue' => $this->promptYesNo('Is this argument required? ', false),
+                    'defaultValue' => $this->prompt('Please provide a default value [N/A]: ')
+                ];
 
-            $adding = $this->promptYesNo('Do you want to define another argument? [Yes/no]', true);
+                $adding = $this->promptYesNo('Do you want to define another argument? ', true);
+            }
         }
 
         if ($count = count($options)) {
+            $optionsString = 'array_merge($options, [';
             foreach ($options as $key => $option) {
                 $optionsString .= '
             new PhpCli\Option(
@@ -110,9 +117,11 @@ class Hatchery extends Application
                     $optionsString .= "\n\t";
                 }
             }
+            $optionsString .= "\n\t\t".'])';
         }
 
         if ($count = count($arguments)) {
+            $argumentsString = 'array_merge($arguments, [';
             foreach ($arguments as $key => $argument) {
                 $argumentsString .= '
             new PhpCli\Argument(
@@ -126,6 +135,7 @@ class Hatchery extends Application
                     $argumentsString .= "\n\t";
                 }
             }
+            $argumentsString .= "\n\t\t".'])';
         }
 
         if ($phpVersionMinimum) {
@@ -157,6 +167,40 @@ define('SCRIPT', basename(__FILE__));
 
 require_once('$path/vendor/autoload.php');
 
+use PhpCli\Command;
+use PhpCli\Cursor as Cursor;
+
+\$error = null;
+
+pcntl_async_signals(true);
+pcntl_signal(SIGINT, 'quit');
+pcntl_signal(SIGINT, 'quit');
+register_shutdown_function('cleanup');
+set_error_handler(function (\$errno, \$errstr, \$errfile, \$errline) {
+    if (0 === error_reporting()) {
+        return false;
+    }
+    throw new \Exception(\$errstr.' in '.\$errfile.':'.\$errline."\\n\\tException thrown");
+});
+function cleanup()
+{
+    global \$app, \$error;
+    
+    Cursor::show();
+
+    if (isset(\$app->screen) && \$app->screen !== false) {
+        system('tput rmcup');
+    }
+
+    if (!is_null(\$error)) {
+        print \$error."\\n";
+    }
+}
+function quit(int \$code = 0)
+{
+    exit(\$code);
+}
+
 /**
  * APPLICATION
  */
@@ -164,16 +208,13 @@ final class $className extends PhpCli\Application {
     
     protected string \$script = SCRIPT;
 
-    protected string \$defaultCommand = $commandClassName::class;
+    protected Command \$defaultCommand;
 
-    public function __construct()
-    {
-        parent::__construct(); // required
-
-        \$this->defineMenu(PhpCli\Application::MAIN_MENU, [
-            '1' => 'Get started'
-        ]);
-    }
+    /**
+     * Relative to executable; `pwd`/\$configFile
+     * eg. 'config.json'
+     */
+    public static string \$configFile;
 
     /**
      * Define the binary flags, eg.
@@ -181,18 +222,48 @@ final class $className extends PhpCli\Application {
      * 
      * The -h (or --help) is added in Application::defineOptions().
      */
-    protected function defineOptions(): array
+    protected function defineOptions(array \$options = []): array
     {
-        return parent::defineOptions([$optionsString]);
+        return parent::defineOptions($optionsString);
     }
 
     /**
      * Define the arguments the binary accepts (they will be filled in order they are defined), eg.
      *   binry [options...] source destination
      */
-    protected function defineArguments(): array
+    protected function defineArguments(array \$arguments = []): array
     {
-        return parent::defineArguments([$argumentsString]);
+        return parent::defineArguments($argumentsString);
+    }
+
+    protected function init(): void
+    {
+        parent::init(); // invokes createTables()
+
+        \$this->defineMenu(PhpCli\Application::MAIN_MENU, [
+            '1' => 'Get started'
+        ]);
+
+        \$this->defaultCommand = new $commandClassName(\$this);
+
+        // Startup actions
+        // ...
+    }
+
+    /**
+     * Execute the SQL statements to create new tables. Merge the argument
+     * "\$statements" to preserve any parent SQL statements.
+     * 
+     * @param array
+     */
+    protected function createTables(\$statements = [])
+    {      
+        parent::createTables(array_merge(\$statements, [
+            /*
+            '<CUSTOM SQL STATEMEMT>',
+            '<CUSTOM SQL STATEMEMT>'
+            */
+        ]));
     }
 }
 
@@ -206,14 +277,15 @@ class $commandClassName extends PhpCli\Command
         \$this->line('Press ^C to quit');
 
         do {
-            if (\$selection = \$this->app()->menu(PhpCli\Application::MAIN_MENU)) {
+            if (\$selection = \$this->app()->menu(PhpCli\Application::MAIN_MENU)->prompt()) {
                 \$this->linef('You selected "%s"', \$selection);
             }
         } while (\$selection);
     }
 }
 
-(new $name())->run();
+\$app = new $name();
+\$app->run();
 EOT;
         return $template;
     }
