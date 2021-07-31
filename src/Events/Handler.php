035c93c8e363a7e6e1bf783f54b378c5f4fa5a5d
Allen McCabe <allenmccabe@gmail.com>
2021-07-31 10:29:42 -0700

    Event\Handler refactor.

--- src/Events/Handler.php
+++ src/Events/Handler.php
@@ -1,37 +1,26 @@
 <?php declare(strict_types=1);
 
+use PhpCli\Observers\Observer;
+
 namespace PhpCli\Events;
 
 class Handler
 {
-    private Event $Event;
-
     private $callback;
 
     /**
-     * @param Event $Event
      * @param callable $callback
      */
-    public function __construct(Event $Event, callable $callback)
+    public function __construct(callable $callback)
     {
-        $this->Event = $Event;
         $this->callback = $callback;
-    }
-
-    /**
-     * @param Event $Event
-     * @return bool
-     */
-    public function handles(Event $Event): bool
-    {
-        return get_class($this->Event) === get_class($Event);
     }
 
     /**
      * @param Observer|null
      * @return mixed
      */
-    public function __invoke(Observer $Observer = null)
+    public function __invoke($Observer = null)
     {
         $callback = $this->callback;
         return $callback($Observer);
